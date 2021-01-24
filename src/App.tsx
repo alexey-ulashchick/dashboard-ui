4fa7002d0e0000f3029fff2504236cda4716d03c
Alexey Ulashchick <a.ulashchick@gmail.com>
2021-01-23 23:51:38 -0800

    Testing gRPC communtication to backend services through Envoy proxy.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { SignInUserRequest } from '@alexey-ulashchick/dashboard-protos/protos/AuthService_pb';
+import { AuthServiceClient } from '@alexey-ulashchick/dashboard-protos/protos/AuthService_pb_service';
+import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
 
 function App() {
+
+  const onSuccess = (response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+    if (response.code) {
+      console.log(response);
+      return;
+    }
+
+    const resp = response as GoogleLoginResponse;
+    const grpcSingInRequest = new SignInUserRequest();
+
+    grpcSingInRequest.setEmail(resp.getBasicProfile().getEmail());
+    grpcSingInRequest.setFirstName(resp.getBasicProfile().getGivenName());
+    grpcSingInRequest.setLastName(resp.getBasicProfile().getFamilyName());
+    grpcSingInRequest.setImageUrl(resp.getBasicProfile().getImageUrl());
+
+    const client = new AuthServiceClient("http://localhost:8080");
+
+    client.signIn(grpcSingInRequest, (err, response) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        console.log(response?.getJwttoken());
+    });
+  };
+
+  const onFailure = (failure: {}) => {
+    console.error(failure);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+        clientId="631305080500-ikvt7r4vgsnsicvc1mo810atleqcgvu2.apps.googleusercontent.com"
+        buttonText="Login with Google"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={false}
+      />
     </div>
   );
 }
